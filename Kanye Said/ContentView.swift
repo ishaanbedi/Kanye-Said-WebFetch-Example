c266f96f9c09b40308ba6e4d6a943c17ead221e0
Ishaan Bedi <bediishaan6@gmail.com>
2023-02-25 11:47:52 +0530

    Initial Commit

---
+++ Kanye Said/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Kanye Said
+//
+//  Created by Ishaan Bedi on 25/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kanye Said/Kanye_SaidApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kanye_SaidApp.swift
+//  Kanye Said
+//
+//  Created by Ishaan Bedi on 25/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Kanye_SaidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
