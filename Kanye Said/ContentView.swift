edbf05902928ca6e8ece29c4b5ec93def4b961fc
Ishaan Bedi <bediishaan6@gmail.com>
2023-02-25 12:18:38 +0530

    Upload Final Application

--- Kanye Said/ContentView.swift
+++ Kanye Said/ContentView.swift
@@ -1,21 +1,54 @@
-//
-//  ContentView.swift
-//  Kanye Said
-//
-//  Created by Ishaan Bedi on 25/02/23.
-//
-
 import SwiftUI
+import WebFetch
 
 struct ContentView: View {
+    @State private var quote: String?
+    @State private var disableButton = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            if let quote = quote {
+                VStack {
+                    Text("Kanye once said:")
+                    Text("*\(quote)*.")
+                        .font(.title2)
+                        .padding(.vertical)
+                }
+            } else {
+                Text("Loading...")
+            }
+            Spacer()
+            Button(action: generateAnotherQuote) {
+                Text("Another One")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .disabled(disableButton)
+            .opacity(disableButton ? 0.1 : 1.0)
+            .padding(.all)
         }
-        .padding()
+        .onAppear {
+            generateAnotherQuote()
+        }
+    }
+    
+    func generateAnotherQuote() {
+        WebFetch().fetch(url: "https://api.kanye.rest/") { result in
+            switch result {
+            case .success(let json):
+                if let jsonObject = json as? [String: Any], let quote = jsonObject["quote"] as? String {
+                    DispatchQueue.main.async {
+                        self.quote = quote
+                    }
+                    disableButton = false
+                }
+            case .failure(let error):
+                print("Error fetching: \(error.localizedDescription)")
+            }
+        }
     }
 }
 
